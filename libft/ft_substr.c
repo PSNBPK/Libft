41b721b25954f7f7086dfa23a9069f7291c2148f
Patsanai Boonprok <135139098+PSNBPK@users.noreply.github.com>
2024-10-16 17:27:05 +0200

    Add files via upload
    add new file with synopsis and hearder file

---
+++ libft/ft_substr.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_substr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: patboonp <patboonp@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/10/16 17:19:28 by patboonp      #+#    #+#                 */
+/*   Updated: 2024/10/16 17:20:18 by patboonp      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+cahr *ft_substr(const char *s, unsigned int start, size_t len)
+{
+    
+}
